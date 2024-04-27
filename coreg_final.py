036d4ae4f99ca3e8c8b60cacfc58eb8461ba3a61
von0221 <aobensss@gmail.com>
2024-04-27 22:21:45 +0800

    提交信息

--- coreg_final.py
+++ coreg_final.py
@@ -41,6 +41,7 @@ def has_digit(line):
             return True
     return False
 
+
 def read_data_from_folder(data_dir):
     files_read = 0
     id1 = []
@@ -93,21 +94,15 @@ def read_data_from_folder(data_dir):
             for i in range(len(words)):
                 if words[i].endswith("/wd"):
                     continue
-             
+
                 id1.append(sentence_id)
-                
+
                 p_front.append(pronoun_front)
-                
-                
+
                 p_behind.append(pronoun_behind)
-                if i == antecedent_front:
-                    a_front.append(1)
-                else:
-                    a_front.append(0)
-                if i == antecedent_behind:
-                    a_behind.append(1)
-                else:
-                    a_behind.append(0)
+
+                a_front.append(antecedent_front)
+                a_behind.append(antecedent_behind)
                 if i >= antecedent_front and i <= antecedent_behind:
                     a_in.append(1)
                 else:
@@ -126,11 +121,12 @@ def read_data_from_folder(data_dir):
 
     return id2
 
+
 def cidai(id_t, feature_df):
     print(1)
     id_t_with_one_hot = id_t.copy()
     word_counts = id_t['word'].value_counts()
-    
+
     # 创建一个空的DataFrame来存储匹配情况
     match_df_list = []
 
@@ -148,15 +144,15 @@ def cidai(id_t, feature_df):
     # 将匹配情况合并到id_t_with_one_hot中
     id_t_with_one_hot = pd.concat([id_t_with_one_hot.iloc[:, 1:], match_df], axis=1)
 
-    # 保留至少有一个匹配的行
-    id_t_with_one_hot = id_t_with_one_hot[id_t_with_one_hot[selected_features.columns].sum(axis=1) > 0]
+    # # 保留至少有一个匹配的行
+    # id_t_with_one_hot = id_t_with_one_hot[id_t_with_one_hot[selected_features.columns].sum(axis=1) > 0]
 
     print(id_t_with_one_hot)
     return id_t_with_one_hot
 
 
 def oversample_data(data, target_column):
-    X = data.iloc[:, [0, 1, 2, 5] + [i for i in range(8, 5011)]]
+    X = data.iloc[:, [0, 1, 2, 3, 4] + [i for i in range(8, 5012)]]
     y = data[target_column]
     oversampler = RandomOverSampler(random_state=0)
     X_resampled, y_resampled = oversampler.fit_resample(X, y)
@@ -203,28 +199,46 @@ def count_overlap(list1, list2):
         for start2, end2 in list2:
             if start1 == start2 and end1 ==end2:  # 判断两个连续部分是否有重叠
                 count += 1
+                break
     return count
 
 
+def return_overlap(list1, list2):
+    continuous_indices1 = []
+    for start1, end1 in list1:
+        for start2, end2 in list2:
+            if start1 == start2 and end1 ==end2:  # 判断两个连续部分是否有重叠
+                continuous_indices1.append((start1, end1))
+                break
+    return continuous_indices1
 
+
+import torch.nn.functional as F
 
 
 class ImprovedMLP(nn.Module):
-    def __init__(self, input_size, hidden_size1, hidden_size2):
+    def __init__(self, input_size, hidden_size1, hidden_size2, hidden_size3):
         super(ImprovedMLP, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size1)
         self.fc2 = nn.Linear(hidden_size1, hidden_size2)
-        self.fc3 = nn.Linear(hidden_size2, 1)
+        self.fc3 = nn.Linear(hidden_size2, hidden_size3)
+        self.fc4 = nn.Linear(hidden_size3, 1)
         self.dropout1 = nn.Dropout(0.5)
         self.dropout2 = nn.Dropout(0.5)
-        
+        self.dropout3 = nn.Dropout(0.5)
+
     def forward(self, x):
         x = F.leaky_relu(self.fc1(x))
         x = self.dropout1(x)
         x = F.leaky_relu(self.fc2(x))
         x = self.dropout2(x)
-        x = torch.sigmoid(self.fc3(x))
+        x = F.leaky_relu(self.fc3(x))
+        x = self.dropout3(x)
+        x = torch.sigmoid(self.fc4(x))
         return x
+
+
+
 
 
 
@@ -254,7 +268,7 @@ if __name__ == "__main__":
     for i in seg_sen:
         for j in i:
         # 利用负向前瞻排除指定后缀的词
-            m = re.findall(r'[\u4e00-\u9fff](?:(?!/t|/wd|/wu|/wj|/d|/p|/ul|/vl|不|/f|\vl).)*$', j)
+            m = re.findall(r'[\u4e00-\u9fff](?:(?!/t|/wd|/wu|/wj|/d|/p|/ul|/vl|不|/f|/vl|/vt|/vi).)*$', j)
         # 如果 m 不为空，说明 j 符合条件，将其添加到 key 中
             if m:
                 key.append(m)
@@ -312,19 +326,21 @@ if __name__ == "__main__":
 
     id_e = read_data_from_folder("test")
     # 检查 word 列中的字符串值是否以 "\rr" 为后缀，并创建新的标签列
-    
 
     id_t['\rr'] = id_t['word'].str.endswith('/rr').astype(int)
     id_t['\nrg'] = id_t['word'].str.endswith('/nrg').astype(int)
     id_t['\nrf'] = id_t['word'].str.endswith('/nrf').astype(int)
+    id_t['\n'] = id_t['word'].str.endswith('/n').astype(int)
     # 打印修改后的 DataFrame
     print(id_t)
-    id_e['\rr'] = id_t['word'].str.endswith('/rr').astype(int)
-    id_v['\rr'] = id_t['word'].str.endswith('/rr').astype(int)
+    id_e['\rr'] = id_e['word'].str.endswith('/rr').astype(int)
+    id_v['\rr'] = id_v['word'].str.endswith('/rr').astype(int)
     id_e['\nrg'] = id_e['word'].str.endswith('/nrg').astype(int)
     id_e['\nrf'] = id_e['word'].str.endswith('/nrf').astype(int)
     id_v['\nrg'] = id_v['word'].str.endswith('/nrg').astype(int)
     id_v['\nrf'] = id_v['word'].str.endswith('/nrf').astype(int)
+    id_e['\n'] = id_e['word'].str.endswith('/n').astype(int)
+    id_v['\n'] = id_v['word'].str.endswith('/n').astype(int)
 
     id_t_with_one_hot = cidai(id_t, feature_df)
     id_e_with_one_hot = cidai(id_e, feature_df)
@@ -345,8 +361,8 @@ if __name__ == "__main__":
 
     print(X_train_resampled3)
 
-    X_train_values3 = X_train_resampled3.iloc[:, [1, 2] + [i for i in range(4, 5007)]].values.astype('float32')
-    X_vali_values3 = X_vali_resampled3.iloc[:, [1, 2] + [i for i in range(4, 5007)]].values.astype('float32')
+    X_train_values3 = X_train_resampled3.iloc[:, [i for i in range(1, 5009)]].values.astype('float32')
+    X_vali_values3 = X_vali_resampled3.iloc[:, [i for i in range(1, 5009)]].values.astype('float32')
     X_train3 = torch.tensor(X_train_values3)
 
     X_vali3 = torch.tensor(X_vali_values3)
@@ -354,15 +370,19 @@ if __name__ == "__main__":
     Y_train3 = torch.tensor(y_train_resampled3.values.astype('float32'))
     Y_vali3 = torch.tensor(y_vali_resampled3.values.astype('float32'))
 
-    class_counts9 = id_e_with_one_hot.groupby('id').size().reset_index(name='Count').shape[0]
+    class_counts9 = X_vali_resampled3.groupby('id').size().reset_index(name='Count').shape[0]
     print(class_counts9)
     # 模型初始化
-    input_size = 5005
-    hidden_size1 = 128
-    hidden_size2 = 64
-    improved_model = ImprovedMLP(input_size, hidden_size1, hidden_size2)
+    # 模型初始化
+    input_size = 5008
+    hidden_size1 = 256
+    hidden_size2 = 128
+    hidden_size3 = 64
+    improved_model = ImprovedMLP(input_size, hidden_size1, hidden_size2, hidden_size3)
+    # weights = torch.tensor([1, 100])
+    # criteria = nn.CrossEntropyLoss(weight=weights)
 
-    # 定义损失函数和优化器
+    # # 定义损失函数和优化器
     criteria = nn.BCELoss()
     optimizer = optim.Adam(improved_model.parameters(), lr=0.001, weight_decay=0.001)
     # Train Process
@@ -370,13 +390,43 @@ if __name__ == "__main__":
     loss_val = []
     accuracy_event1_train = []
     accuracy_event1_val = []
-    for iteration in range(1000):
+    for iteration in range(400):
         # Forward Propagation
         y_pred = improved_model(X_train3)
-        y_valipred = improved_model(X_vali3)
+        # y_valipred = improved_model(X_vali3)
         # Loss Calculating
         loss1 = criteria(y_pred.squeeze(), Y_train3)
-        loss2 = criteria(y_valipred.squeeze(), Y_vali3)
+        # loss2 = criteria(y_valipred.squeeze(), Y_vali3)
+
+        # 初始化空列表来存储所有的特征和标签
+        X_finalvali1_list = []
+        Y_finalvali1_list = []
+
+        # 设置每次追加的数据批次大小
+        batch_size = 6400
+
+        # 计算总共需要追加多少次批次
+        total_batches = len(id_v_with_one_hot) // batch_size
+        class_counts2 = 0
+        # 迭代读取数据并追加到列表中
+        for i in range(total_batches):
+            start_idx = i * batch_size
+            end_idx = min((i + 1) * batch_size, len(id_v_with_one_hot))  # 避免超出索引范围
+            X_batch = id_v_with_one_hot.iloc[start_idx:end_idx,
+                      [1, 2, 3, 4] + [i for i in range(8, 5012)]].values.astype(
+                'float32')
+            Y_batch = id_v_with_one_hot['a_in'].iloc[start_idx:end_idx].values.astype('float32')
+
+            # 将批次数据追加到列表中
+            X_finalvali1_list.append(X_batch)
+            Y_finalvali1_list.append(Y_batch)
+            class_counts2 += id_v_with_one_hot.iloc[start_idx:end_idx].groupby('id').size().reset_index(name='Count').shape[0]
+        # 将列表转换为张量
+        X_finalvali1 = torch.tensor(np.concatenate(X_finalvali1_list, axis=0))
+        Y_finalvali1 = torch.tensor(np.concatenate(Y_finalvali1_list, axis=0))
+
+        y_valipred = improved_model(X_finalvali1)
+        loss2 = criteria(y_valipred.squeeze(), Y_finalvali1)
         print('iteration:{}'.format(iteration))
         print('loss_train:{}'.format(loss1.item()))
         print('loss_val:{}'.format(loss2.item()))
@@ -385,10 +435,14 @@ if __name__ == "__main__":
 
         # Accuracy Calculating
         f1_measure_train = sklearn.metrics.f1_score(Y_train3.ge(0.5).float(), y_pred.ge(0.5).float())
-        f1_measure_val = sklearn.metrics.f1_score(Y_vali3.ge(0.5).float(), y_valipred.ge(0.5).float())
+        f1_measure_val = sklearn.metrics.f1_score(Y_finalvali1.ge(0.5).float(), y_valipred.ge(0.5).float())
         if iteration % 10 == 0:
-            print('F1-measure-validation:{}'.format(f1_measure_val))
             print('F1-measure-validation:{}'.format(f1_measure_train))
+            print('F1-measure-validation:{}'.format(f1_measure_val))
+
+
+
+
 
         accuracy_event1_train.append(f1_measure_train)
         accuracy_event1_val.append(f1_measure_val)
@@ -398,6 +452,81 @@ if __name__ == "__main__":
         loss1.mean().backward()  # 使用平均损失进行反向传播
 
         optimizer.step()
+
+    # 初始化空列表来存储所有的特征和标签
+    X_finaltest1_list = []
+    Y_finaltest1_list = []
+
+    # 设置每次追加的数据批次大小
+    batch_size = 6400
+
+    # 计算总共需要追加多少次批次
+    total_batches = len(id_e_with_one_hot) // batch_size
+    class_counts1 = 0
+    # 迭代读取数据并追加到列表中
+    for i in range(total_batches):
+        start_idx = i * batch_size
+        end_idx = min((i + 1) * batch_size, len(id_e_with_one_hot))  # 避免超出索引范围
+        X_batch = id_e_with_one_hot.iloc[start_idx:end_idx, [1, 2, 3, 4] + [i for i in range(8, 5012)]].values.astype(
+            'float32')
+        Y_batch = id_e_with_one_hot['a_in'].iloc[start_idx:end_idx].values.astype('float32')
+
+        # 将批次数据追加到列表中
+        X_finaltest1_list.append(X_batch)
+        Y_finaltest1_list.append(Y_batch)
+        class_counts1 += id_e_with_one_hot.iloc[start_idx:end_idx].groupby('id').ngroups
+
+    # 将列表转换为张量
+    X_finaltest1 = torch.tensor(np.concatenate(X_finaltest1_list, axis=0))
+    Y_finaltest1 = torch.tensor(np.concatenate(Y_finaltest1_list, axis=0))
+
+    y_final_pred = improved_model(X_finaltest1)
+    loss_test = criteria(y_final_pred.squeeze(), Y_finaltest1)
+    print()
+    print('loss_test:{}'.format(loss_test.item()))
+    a_in_list = id_e_with_one_hot['a_fron'].tolist()
+    a_be_list = id_e_with_one_hot['a_be'].tolist()
+
+    # 将两个列表组合成列表对的形式存储
+    label_pairs = list(zip(a_in_list, a_be_list))
+    continuous_ones_indices_test = find_continuous_indices(y_final_pred)
+    continuous_ones_indices_test_label = find_continuous_indices(Y_finaltest1)
+    # 输出连续为1的词的连续行数的序号
+    overlap_count = count_overlap(continuous_ones_indices_test, continuous_ones_indices_test_label)
+
+    precition2 = overlap_count / class_counts1
+    num_rows = len(continuous_ones_indices_test_label)
+    print(num_rows)
+    num_rows2 = len(continuous_ones_indices_test)
+    print(num_rows2)
+    precition1 = overlap_count / num_rows
+
+    print('precition on test set:', precition2)
+    print('precition on test set:', precition1)
+
+    # 找到 预测值和真实值 中值都为 1 的行的索引
+
+    resultlines = return_overlap(continuous_ones_indices_test, continuous_ones_indices_test_label)
+    for start, end in resultlines:
+        # 提取开始与结尾之间的行
+        selected_rows = id_e_with_one_hot.iloc[start:end + 1]  # 加1是因为结束索引是不包含在内的
+
+        # 打印每一行的 id 列和 word 列
+        for idx, row in selected_rows.iterrows():
+            print("ID: {}, Word: {}, NO:{}".format(row['id'], row['word'], row['se']))
+
+    # indices_of_ones = [idx for idx, val in enumerate(y_final_pred) if val > 0.5]
+    # print(indices_of_ones)
+    # # 从 id_e_with_one_hot 中取出所有需要的行
+    # selected_rows = id_e_with_one_hot.iloc[indices_of_ones]
+    #
+    # # 打印每一行的 "word" 标签
+    # for idx, row in selected_rows.iterrows():
+    #     word = row['word']
+    #     id1 = row['id']
+    #     print("Id: {}, Word: {}".format(id1, word))
+
+# Model Evaluation
 
 
 
@@ -497,19 +626,3 @@ if __name__ == "__main__":
 #         for param_group_ in optimizer.param_groups:
 #             param_group_['lr'] = lr_
 
-
-    X_finaltest1 = torch.tensor(
-        id_e_with_one_hot.iloc[:, [1, 2] + [i for i in range(8, 5011)]].values.astype('float32'))
-    Y_finaltest1 = torch.tensor(id_e_with_one_hot['a_in'].values.astype('float32'))
-
-    y_final_pred = improved_model(X_finaltest1)
-    count_total2 = 0
-    continuous_ones_indices_test = find_continuous_indices(y_final_pred)
-    continuous_ones_indices_test_label = find_continuous_indices(Y_finaltest1)
-    # 输出连续为1的词的连续行数的序号
-    overlap_count = count_overlap(continuous_ones_indices_test, continuous_ones_indices_test_label)
-    count_total2 = count_total2 + overlap_count
-
-    precition2 = count_total2 / class_counts9
-    print('precition on validation set:', precition2)
-# Model Evaluation
